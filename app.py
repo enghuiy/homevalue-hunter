4673672b19363550d154f4a2e6cbd099b1b259ba
Eng Hui Yap <enghuiy@enghuiMac.home>
2016-02-17 19:45:20 -0500

    -

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def index():
 def graph():
 
   myData=pd.read_csv('myData.csv')
-  plot = figure(title='Median Home Prices vs. SAT Score',
+  plot = figure(myData['Mean Total SAT'],myData['Median Home Sale Price'],title='Median Home Prices vs. SAT Score',
                 x_axis_label='SAT Score')
 
   
