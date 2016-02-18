c826764fc6a264a26da879afac7efbd52ea79bd3
Eng Hui Yap <enghuiy@enghuiMac.home>
2016-02-17 19:30:48 -0500

    -

--- app_orig.py
+++ app_orig.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, request, redirect
-import csv
-import pandas as pd
 
 app = Flask(__name__)
 
@@ -11,11 +9,6 @@ def main():
 @app.route('/index')
 def index():
   return render_template('index.html')
-
-
-myData=pd.read_csv('myData.csv')
-
-#predHousePrice_bySAT
 
 if __name__ == '__main__':
   app.run(port=33507)
