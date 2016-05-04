522ed5badb6c552689658a8b7ad240d905c878f5
Enghui Yap <enghuiy@gmail.com>
2016-05-04 06:50:08 -0400

    -

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 from __future__ import division
 from flask import Flask, render_template, request, redirect
 
-import os
-import psycopg2
-import urlparse
+#import os
+#import psycopg2
+#import urlparse
 
-import math
-import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import Imputer
+#import math
+#import numpy as np
+#from sklearn.linear_model import LinearRegression
+#from sklearn.preprocessing import Imputer
 
-from bokeh.plotting import *
-from bokeh.embed import components
-from bokeh.models import HoverTool,sources
-from collections import OrderedDict
+#from bokeh.plotting import *
+#from bokeh.embed import components
+#from bokeh.models import HoverTool,sources
+#from collections import OrderedDict
 
 #from wordcloud import WordCloud
 #import matplotlib.pyplot as plt
 #import mpld3
 #import json
 
-@app.route('/index', methods=['POST', 'GET'])
+#@app.route('/index', methods=['POST', 'GET'])
+@app.route('/index')
 def index():
   return render_template('index.html',msg='No such locales found')
 
